35295ed07650b5da8c55e2e63b9ec4f6f87a5388
Ron Geva <coolron54@gmail.com>
2025-03-24 18:47:10 +0200

    Parallelism: Improved HW1 (to 50 milliseconds) by using normal merge
    Changed the code to use a regular merge procedure instead of the
    odd-even merge.
    Also used the "shared" clause in the OpenMP preprocessor commands to
    avoid copying the numbers vector each time we run something in parallel.
    
    Current execution time is fast, but can be improved further.

--- Parallelism/Parallelism/HW1/main.cpp
+++ Parallelism/Parallelism/HW1/main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <string>
 #include <cstdlib>  // For std::stoi (C++11 and later)
+#include <cstring>
 #include <vector>
 #include <algorithm>
 #include <omp.h>
@@ -13,56 +14,8 @@
 
 using std::vector;
 using namespace std::chrono;
-
-void* g_buffers = NULL;
-//#define ODD_EVEN_MERGE
-
-void preallocate_buffers(size_t biggest_power)
-{
-	const size_t n = (1LL << biggest_power);
-
-	g_buffers = malloc(sizeof(void*) * (biggest_power + 1));
-
-	for (size_t power = 0; power <= biggest_power; power++)
-	{
-		// allocate n/2^i vectors of size 2^i
-		uint64_t maximum_parallel_buffers = (n >> power) * 32;
-		void* buffer = malloc(sizeof(void*) * maximum_parallel_buffers);
-		for (size_t j = 0; j < maximum_parallel_buffers; j++)
-		{
-			reinterpret_cast<vector<uint64_t>**>(buffer)[j] =
-				new vector<uint64_t>(1LL << power);
-		}
-
-		reinterpret_cast<void**>(g_buffers)[power] = buffer;
-	}
-}
-
-vector<uint64_t>& get_preallocated_buffer(size_t power)
-{
-	static uint64_t indexes[64] = { 0 };
-
-	uint64_t index;
-	#pragma omp critical
-	{
-		index = indexes[power]++;
-	}
-
-	return *reinterpret_cast<vector<uint64_t>***>(g_buffers)[power][index];
-}
-
-// retrieves log2 of a power of 2 number
-size_t fast_log2(size_t size)
-{
-	size_t power = 0;
-	while (size > 1)
-	{
-		size >>= 1;
-		power++;
-	}
-
-	return power;
-}
+uint64_t* g_buffer1 = NULL;
+uint64_t* g_buffer2 = NULL;
 
 void pad_to_power_of_two(vector<uint64_t>& arr, uint64_t value)
 {
@@ -74,79 +27,64 @@ void pad_to_power_of_two(vector<uint64_t>& arr, uint64_t value)
 	arr.resize(size, value);
 }
 
-void odd_even_merge(vector<uint64_t>& left, vector<uint64_t>& right, vector<uint64_t>& merged_result)
+void odd_even_merge(vector<uint64_t>& arr, size_t start, size_t step, size_t len)
 {
-	size_t sublist_size = left.size();
-	if (sublist_size == 1)
-	{
-		merged_result[0] = min(left[0], right[0]);
-		merged_result[1] = max(left[0], right[0]);
+	if (len == 1)
 		return;
-	}
 
-	size_t power = fast_log2(sublist_size / 2);
-	vector<uint64_t>& left_even_copy = get_preallocated_buffer(power);
-	vector<uint64_t>& left_odd_copy = get_preallocated_buffer(power);
-	vector<uint64_t>& right_even_copy = get_preallocated_buffer(power);
-	vector<uint64_t>& right_odd_copy = get_preallocated_buffer(power);
+	#pragma omp task shared(arr)
+	// odd
+	odd_even_merge(arr, start + step, 2 * step, len / 2);
 
-	#pragma omp parallel for
-	for (size_t i = 0; i < sublist_size / 2; i++)
+	#pragma omp task shared(arr)	
+	// even
+	odd_even_merge(arr, start, 2 * step, len / 2);
+
+	#pragma omp taskwait
+
+	// assume each half of the subarray is sorted, merge them
+	#pragma omp parallel for shared(arr)
+	for (size_t i = 0; i < len - 1; i++)
 	{
-		left_even_copy[i] = left[2 * i];
-		left_odd_copy[i] = left[2 * i + 1];
-
-		right_even_copy[i] = right[2 * i];
-		right_odd_copy[i] = right[2 * i + 1];
-	}
-
-	vector<uint64_t>& c = get_preallocated_buffer(power + 1);
-	vector<uint64_t>& d = get_preallocated_buffer(power + 1);
-	#pragma omp parallel
-	{
-		#pragma omp single nowait
+		if (arr[start + i * step] > arr[start + (i + 1) * step])
 		{
-			#pragma omp task
-			odd_even_merge(left_even_copy, right_even_copy, c);
-
-			#pragma omp task
-			odd_even_merge(left_odd_copy, right_odd_copy, d);
-
-			#pragma omp taskwait
+			std::swap(arr[start + i * step], arr[start + (i + 1) * step]);
 		}
 	}
+}
 
-	// interleave c and d
-	#pragma omp parallel for
-	for (size_t i = 0; i < sublist_size; i++)
+void merge(vector<uint64_t>& arr, uint64_t l, uint64_t r)
+{
+	size_t len = r - l;
+	memcpy(g_buffer1, &arr[l], (len / 2) * sizeof(uint64_t));
+	memcpy(g_buffer2, &arr[l + len / 2], (len / 2) * sizeof(uint64_t));
+
+	size_t i = 0, j = 0, index = 0;
+	while (i + j < len)
 	{
-		merged_result[2 * i] = c[i];
-		merged_result[2 * i + 1] = d[i];
-	}
+		if ((j == len / 2) ||
+			((i < len / 2) && (g_buffer1[i] < g_buffer2[j])))
+		{
+			// choose left element
+			arr[l + index] = g_buffer1[i];
+			i++;
+		}
+		else
+		{
+			arr[l + index] = g_buffer2[j];
+			j++;
+		}
 
-	// swap neighbors if their order is reversed, this is thread safe
-	#pragma omp parallel for
-	for (size_t i = 0; i < sublist_size * 2 - 1; i++)
-	{
-		if (merged_result[i] <= merged_result[i + 1])
-			continue;
-
-		// swap
-		uint64_t tmp = merged_result[i];
-		merged_result[i] = merged_result[i + 1];
-		merged_result[i + 1] = tmp;
+		index++;
 	}
 }
 
 // sorts the interval arr[l:r] in place
 void parallel_mergesort_internal(vector<uint64_t>& arr, uint64_t l, uint64_t r)
 {
-	if (r - l == 2)
+	if (r - l <= 1)
 	{
-		uint64_t min_val = min(arr[l], arr[l + 1]);
-		uint64_t max_val = max(arr[l], arr[l + 1]);
-		arr[l] = min_val;
-		arr[l + 1] = max_val;
+		// a single element subarray, already sorted
 		return;
 	}
 
@@ -154,64 +92,16 @@ void parallel_mergesort_internal(vector<uint64_t>& arr, uint64_t l, uint64_t r)
 
 	// recursively call merge procedure for the two sublists
 	// use OpenMP to speed things up
-	#pragma omp parallel
-	{
-		#pragma omp single nowait
-		{
-			#pragma omp task
-			parallel_mergesort_internal(arr, l, mid);
-
-			#pragma omp task
-			parallel_mergesort_internal(arr, mid, r);
-
-			#pragma omp taskwait
-		}
-	}
+	#pragma omp task shared(arr)
+	parallel_mergesort_internal(arr, l, mid);
 	
-	// merge the two lists
-	uint64_t i = l, j = mid, k = 0;
-	uint64_t sublist_size = (r - l) / 2;
+	#pragma omp task shared(arr)
+	parallel_mergesort_internal(arr, mid, r);
 
-	// use pre-allocated buffers to hold copies of the 2 sorted sublists
-	// copy them using parallelism
-	size_t power = fast_log2(sublist_size);
+	#pragma omp taskwait
 
-	vector<uint64_t>& left_copy = get_preallocated_buffer(power);
-	vector<uint64_t>& right_copy = get_preallocated_buffer(power);
-
-#pragma omp parallel for
-	for (size_t i = 0; i < sublist_size; i++)
-	{
-		left_copy[i] = arr[l + i];
-		right_copy[i] = arr[mid + i];
-	}
-#ifdef ODD_EVEN_MERGE
-
-	vector<uint64_t>& merged_result = get_preallocated_buffer(power + 1);
-	odd_even_merge(left_copy, right_copy, merged_result);
-#pragma omp parallel for
-	for (size_t i = 0; i < sublist_size * 2; i++)
-	{
-		arr[l + i] = merged_result[i];
-	}
-#else
-	// merge the two copies back into arr
-	size_t a = 0, b = 0;
-	for (size_t i = 0; i < sublist_size * 2; i++)
-	{
-		if (b == sublist_size ||
-			(a < sublist_size && left_copy[a] < right_copy[b]))
-		{
-			arr[l + i] = left_copy[a];
-			a++;
-		}
-		else
-		{
-			arr[l + i] = right_copy[b];
-			b++;
-		}
-	}
-#endif
+	//odd_even_merge(arr, l, 1, r - l);
+	merge(arr, l, r);
 }
 
 void parallel_mergesort(vector<uint64_t>& arr, uint64_t& duration)
@@ -221,9 +111,9 @@ void parallel_mergesort(vector<uint64_t>& arr, uint64_t& duration)
 	// we need the size of the vector to be a power of 2
 	pad_to_power_of_two(arr, (uint64_t)-1);
 
-	size_t biggest_power = fast_log2(arr.size()) + 1;
-
-	preallocate_buffers(biggest_power);
+	// allocate temporary buffers to speed up code
+	g_buffer1 = reinterpret_cast<uint64_t*>(malloc(sizeof(uint64_t) * arr.size()));
+	g_buffer2 = reinterpret_cast<uint64_t*>(malloc(sizeof(uint64_t) * arr.size()));
 
 	auto start = high_resolution_clock::now();
 	parallel_mergesort_internal(arr, 0, arr.size());
